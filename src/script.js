1426161e7198b4c405b31946ef4504a63de3d0ed
Hannes Bröhl <hannes.broehl@icloud.com>
2024-02-09 14:49:12 +0100

    add variable

--- src/script.js
+++ src/script.js
@@ -1,6 +1,18 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
+let rows = 10;
+let cols = 10;
+let snake = [
+  { x: 2, y: 3 },
+  { x: 3, y: 3 },
+  { x: 4, y: 3 },
+];
+let food = { x: 5, y: 5 };
+let cellWidth = canvas.width / cols;
+let cellHeight = canvas.height / rows;
+let direction = "LEFT";
+
 draw();
 function draw() {
   ctx.fillStyle = "lightsalmon";
